9daf56f29374b735aad5ea8824dd3f24fee50e23
buzzdecafe <m_hur@yahoo.com>
2016-06-04 14:07:58 -0400

    add minified and unminified dist versions

--- dist/rdom.js
+++ dist/rdom.js
@@ -1,3 +1,32 @@
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.rdom = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
+module.exports = [
+  "div", 
+  "p", 
+  "table", 
+  "tr", 
+  "td", 
+  "th", 
+  "tbody", 
+  "thead", 
+  "tfoot", 
+  "span", 
+  "ul", 
+  "ol", 
+  "li",
+  "a", 
+  "select", 
+  "option", 
+  "input", 
+  "button", 
+  "h1", 
+  "h2", 
+  "h3", 
+  "h4", 
+  "textarea", 
+  "label"
+];
+
+},{}],"/src/rdom":[function(require,module,exports){
 var tags = require('./tags');
 var elProps = ["className", "innerHTML", "name", "title"];
 
@@ -70,3 +99,6 @@ rdom.addTags = function _rdomAddTags(ts, dest) {
 rdom.addTags(tags, rdom);
 
 module.exports = rdom;
+
+},{"./tags":1}]},{},[])("/src/rdom")
+});
