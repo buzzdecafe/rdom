c499ebbbb08be4862d090ef05e6bf2f6992ed5c2
craigdallimore <decoy9697@gmail.com>
2016-09-26 21:52:03 +0100

    Add and run derequire

--- dist/rdom.js
+++ dist/rdom.js
@@ -1,5 +1,33 @@
-(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.rdom = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(require,module,exports){
-var tags = require('./tags');
+(function(f){if(typeof exports==="object"&&typeof module!=="undefined"){module.exports=f()}else if(typeof define==="function"&&define.amd){define([],f)}else{var g;if(typeof window!=="undefined"){g=window}else if(typeof global!=="undefined"){g=global}else if(typeof self!=="undefined"){g=self}else{g=this}g.rdom = f()}})(function(){var define,module,exports;return (function e(t,n,r){function s(o,u){if(!n[o]){if(!t[o]){var a=typeof require=="function"&&require;if(!u&&a)return a(o,!0);if(i)return i(o,!0);var f=new Error("Cannot find module '"+o+"'");throw f.code="MODULE_NOT_FOUND",f}var l=n[o]={exports:{}};t[o][0].call(l.exports,function(e){var n=t[o][1][e];return s(n?n:e)},l,l.exports,e,t,n,r)}return n[o].exports}var i=typeof require=="function"&&require;for(var o=0;o<r.length;o++)s(r[o]);return s})({1:[function(_dereq_,module,exports){
+module.exports = [
+  'div', 
+  'p', 
+  'table', 
+  'tr', 
+  'td', 
+  'th', 
+  'tbody', 
+  'thead', 
+  'tfoot', 
+  'span', 
+  'ul', 
+  'ol', 
+  'li',
+  'a', 
+  'select', 
+  'option', 
+  'input', 
+  'button', 
+  'h1', 
+  'h2', 
+  'h3', 
+  'h4', 
+  'textarea', 
+  'label'
+];
+
+},{}],"/src/rdom":[function(_dereq_,module,exports){
+var tags = _dereq_('./tags');
 var elProps = ['className', 'innerHTML', 'name', 'title'];
 
 // helper functions
@@ -72,33 +100,5 @@ rdom.addTags(tags, rdom);
 
 module.exports = rdom;
 
-},{"./tags":2}],2:[function(require,module,exports){
-module.exports = [
-  'div', 
-  'p', 
-  'table', 
-  'tr', 
-  'td', 
-  'th', 
-  'tbody', 
-  'thead', 
-  'tfoot', 
-  'span', 
-  'ul', 
-  'ol', 
-  'li',
-  'a', 
-  'select', 
-  'option', 
-  'input', 
-  'button', 
-  'h1', 
-  'h2', 
-  'h3', 
-  'h4', 
-  'textarea', 
-  'label'
-];
-
-},{}]},{},[1])(1)
+},{"./tags":1}]},{},[])("/src/rdom")
 });
